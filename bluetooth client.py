5201f09ce794810d9db9e226fb0d89f43f9a5d6e
benliv493 <benliv2010@gmail.com>
2025-05-12 06:44:52 -0400

    Update bluetooth client.py

--- bluetooth client.py
+++ bluetooth client.py
@@ -4,10 +4,12 @@ import time
 import signal
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect(('a8:93:4a:3c:ce:a4',6))
+client.connect(('f8:fe:5e:1e:eb:e1',6))
 
 trigger_input = threading.Event()
 
+print('Press Ctrl + C to enter a message')
+print('NOTE: Once you do this, you must send your message before you can see incoming messages')
 name = input("Username: ")
 
 print(client.recv(1024).decode())
@@ -16,7 +18,6 @@ client.send((name + ' has joined the chat').encode())
 name = name + ': '
 
 def SignalHandler(sig, frame):
-    print('keyboard detected')
     trigger_input.set()
 
 def listening():
